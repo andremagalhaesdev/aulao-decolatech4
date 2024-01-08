c8dd143aebd3f7838dac81545db6ff52b84d9581
André Magalhães <107771892+andremagalhaesdev@users.noreply.github.com>
2024-01-08 15:06:32 -0300

    Commit Desafio Daniel

--- Manipulando Funções - Daniel/projeto/Program.cs
+++ Manipulando Funções - Daniel/projeto/Program.cs
@@ -4,24 +4,28 @@ class Program
 {
   static void Main()
   {
-    // Solicita o nome do usuário, quilômetros percorridos por dia, 
-    // Horas de uso de eletrônicos por dia e o número de refeições com carne:
     string nome = Console.ReadLine();
     double quilometrosPorDia = double.Parse(Console.ReadLine());
     int horasDeEletronicos = int.Parse(Console.ReadLine());
     int refeicoesComCarne = int.Parse(Console.ReadLine());
 
-    // Chama o método para calcular a pegada de carbono
     double pegadaDeCarbono = CalcularPegadaDeCarbono(quilometrosPorDia, horasDeEletronicos, refeicoesComCarne);
-
-    // TODO: Exiba o resultado para o usuário:
-    Console.WriteLine();
-
-    // Aguarda a entrada do usuário antes de encerrar o programa:
+    Console.WriteLine($"{nome}, sua pegada de carbono e de {pegadaDeCarbono} toneladas de CO2 por ano.");
     Console.ReadLine();
   }
 
-  // TODO: Crie um método/função para calcular a pegada de carbono com base nos parâmetros fornecidos:
+  static double CalcularPegadaDeCarbono(double quilometrosPorDia, int horasDeEletronicos, int refeicoesComCarne)
+  {
+    const double fatorTransporte = 0.2;
+    const double fatorEletronicos = 0.1;
+    const double fatorCarne = 0.5;
+    const int diasNoAno = 365;
 
+    double pegadaTransporte = quilometrosPorDia * diasNoAno * fatorTransporte;
+    double pegadaEletronicos = horasDeEletronicos * fatorEletronicos;
+    double pegadaCarne = refeicoesComCarne * fatorCarne;
 
+    double pegadaTotal = pegadaTransporte + pegadaEletronicos + pegadaCarne;
+    return pegadaTotal;
+  }
 }
