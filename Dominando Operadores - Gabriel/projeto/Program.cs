b2ac85e9f9f2838623b5b26a1b6a0193daa84f42
Gabriel Ferreira <108910389+GabrielBhain@users.noreply.github.com>
2024-01-08 15:03:28 -0300

    Adicionado resolução do desafio

--- Dominando Operadores - Gabriel/projeto/Program.cs
+++ Dominando Operadores - Gabriel/projeto/Program.cs
@@ -1,27 +1,20 @@
-﻿using System;
+using System;
 
 class Program
 {
-  static void Main()
-  {
-
-    // TODO: Crie as variáveis titulo, descricao e dataVencimento para guardar as informações das tarefas:
-
-
-    // Obtém o titulo e a descricao a partir da entrada do console  
-    titulo = Console.ReadLine();
-    descricao = Console.ReadLine();
-
-    // TODO: Crie uma estrutura condicional 'if/else' para verificar se a descrição da tarefa excede 50 caracteres.
-
-
-
-    // Caso a descricao esteja dentro do limite é solicitado a entrada pelo console:
-    dataVencimento = Console.ReadLine();
-
-
-    // TODO: Imprima a descricao e a data de vencimento como nos exemplos da tabela:            
-
-  }
-}
+    static void Main()
+    {
+        string titulo, descricao, dataVencimento;
+        titulo = Console.ReadLine();
+        descricao = Console.ReadLine();
+        dataVencimento = Console.ReadLine();
+        if (descricao.Length > 50)
+        {
+            Console.WriteLine("Descricao ultrapassa limite de caracteres.");
+        }
+        else
+        {
+            Console.WriteLine("{0} ate {1}", descricao, dataVencimento);
+        }
+    }
 }
