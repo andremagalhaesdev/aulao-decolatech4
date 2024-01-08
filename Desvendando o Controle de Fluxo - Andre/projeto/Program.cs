f77b27a30d0dc2254faecf3a31e9a2be6032f9e9
André Magalhães <107771892+andremagalhaesdev@users.noreply.github.com>
2024-01-08 17:12:54 -0300

    Controle de Fluxo Pronto!

--- Desvendando o Controle de Fluxo - Andre/projeto/Program.cs
+++ Desvendando o Controle de Fluxo - Andre/projeto/Program.cs
@@ -4,24 +4,47 @@ class Program
 {
     static void Main()
     {
-        // Solicita ao usuário os limites inferiores e superiores
         int limiteInferior = int.Parse(Console.ReadLine());
         int limiteSuperior = int.Parse(Console.ReadLine());
-
-        // Variável para acumular a soma dos números pares
         int somaPares = 0;
 
-        // TODO: Crie um Loop para percorrer os números no intervalo
-        // Lembre-se: O limiteSuperior deve ser menor ou igual a i;
-
-        // TODO: Implemente o if para verificar se o número é par:
-
-
-        // TODO: Crie o acumulador para a soma dos números pares:
-
-
+        for (int i = limiteInferior; i <= limiteSuperior; i++)
+        {
+            if (i % 2 == 0)
+            {
+                somaPares += i;
+            }
+        }
 
         // Exibe o resultado
         Console.WriteLine($"A soma dos numeros pares de {limiteInferior} a {limiteSuperior} e: {somaPares}");
     }
 }
+
+// RESOLUÇÃO OTIMIZADA 
+
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         int limiteInferior = int.Parse(Console.ReadLine());
+//         int limiteSuperior = int.Parse(Console.ReadLine());
+//         int otimizador = limiteInferior;
+//         int somaPares = 0;
+
+//         if (otimizador % 2 != 0)
+//         {
+//             otimizador++;
+//         }
+
+//         for (int i = otimizador; i <= limiteSuperior; i += 2)
+//         {
+//             somaPares += i;
+//         }
+
+//         Console.WriteLine($"A soma dos numeros pares de {limiteInferior} a {limiteSuperior} e: {somaPares}");
+//     }
+// }
+
