8bf1d8b995e82f63de25ee93b809280bcf0ddf06
Lívia <liviapnd@gmail.com>
2024-01-08 09:52:12 -0300

    resolução do desafio Utilizando Construtores

--- Utilizando Construtores - Livia/projeto/Program.cs
+++ Utilizando Construtores - Livia/projeto/Program.cs
@@ -2,33 +2,41 @@
 
 class Personagem
 {
-  public string Nome { get; set; }
-  public string Raca { get; set; }
-  public string Classe { get; set; }
-  public int Nivel { get; set; } = 1;
-  public int Vida { get; set; } = 10;
+    public string Nome { get; set; }
+    public string Raca { get; set; }
+    public string Classe { get; set; }
+    public int Nivel { get; set; } = 1;
+    public int Vida { get; set; } = 10;
 
-  public Personagem(string nome, string raca, string classe)
-  {
-    Nome = nome;
-    Raca = raca;
-    Classe = classe;
-  }
+    public Personagem(string nome, string raca, string classe)
+    {
+        Nome = nome;
+        Raca = raca;
+        Classe = classe;
+    }
 
-  public void ExibirStatus()
-  {
-    Console.WriteLine("Nome:" + Nome);
-    Console.WriteLine("Raça:" + Raca);
-    Console.WriteLine("Classe:" + Classe);
-    Console.WriteLine("Nível:" + Nivel);
-    Console.WriteLine("Vida:" + Vida);
-  }
+    public void ExibirStatus()
+    {
+        Console.WriteLine("Nome:" + Nome);
+        Console.WriteLine("Raça:" + Raca);
+        Console.WriteLine("Classe:" + Classe);
+        Console.WriteLine("Nível:" + Nivel);
+        Console.WriteLine("Vida:" + Vida);
+    }
 }
 
 class Program
 {
-  static void Main()
-  {
-
-  }
+    static void Main()
+    {
+      string nome = Console.ReadLine(); 
+      string raca = Console.ReadLine();
+      string classe = Console.ReadLine();
+      
+      Personagem personagem = new Personagem(nome, raca, classe);
+      
+      Console.WriteLine("Status:");
+      
+      personagem.ExibirStatus();
+    }
 }
