fae9cf658c9bfae6f41ebc5aeb46e19d02283ac4
LucasAdao <l.adao@outlook.com>
2024-01-08 18:14:10 -0300

    Lucas - Projeto

--- Criando um Objeto com Atributos - Lucas/projeto/Program.cs
+++ Criando um Objeto com Atributos - Lucas/projeto/Program.cs
@@ -7,25 +7,20 @@ class Jogador
     public int Idade { get; set; }
     public string Posicao { get; set; }
 
-    public Jogador()
-    {
-    public string Nome { get; set; }
-    public string Nacionalidade { get; set; }
-    public int Idade { get; set; }
-    public string Posicao { get; set; }
 
     public Jogador(string nome, string nacionalidade, int idade, string posicao)
     {
-     Nome = nome;
-     Nacionalidade = nacionalidade;
-     Idade = idade;
-     Posicao = posicao;
+        Nome = nome;
+        Nacionalidade = nacionalidade;
+        Idade = idade;
+        Posicao = posicao;
     }
-    public void Apresentar(){
-       Console.WriteLine($"Jogador criado!\n {Nome}\n {Nacionalidade} \n {Idade} \n Posição: {Posicao}");
-    }
+    public void Apresentar()
+    {
+        Console.WriteLine($"Jogador criado!\n {Nome}\n {Nacionalidade} \n {Idade} \n Posição: {Posicao}");
     }
 }
+
 
 class Program
 {
