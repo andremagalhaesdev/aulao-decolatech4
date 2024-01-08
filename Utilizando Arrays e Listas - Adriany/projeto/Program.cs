42f67b862649898257ad2a5c7969222a909e08cf
adrianycmc <adrianycmc@gmail.com>
2024-01-08 11:53:47 -0300

    Utilizando Arrays e Listas enviado!

--- Utilizando Arrays e Listas - Adriany/projeto/Program.cs
+++ Utilizando Arrays e Listas - Adriany/projeto/Program.cs
@@ -8,13 +8,14 @@ class Program
         int quantidadeJogos = int.Parse(Console.ReadLine());
 
         // TODO: Inicializa os arrays com base na quantidade informada pelo usuário:
-
+        string[] nomesJogos = new string[quantidadeJogos];
 
         // TODO: Crei um Loop para adicionar jogos conforme a quantidade especificada:
-
-
+        for (int i = 0; i < quantidadeJogos ; i++)
+        {
         //TODO: Chame a função AdicionarJogo para obter o nome do jogo e armazená-lo no array:
-
+            AdicionarJogo(i, nomesJogos);
+        }
 
         // Exibe o resumo da adição de jogos
         ExibirResumoAdicaoJogos(quantidadeJogos, nomesJogos);
@@ -28,6 +29,6 @@ class Program
 
     static void ExibirResumoAdicaoJogos(int quantidadeJogos, string[] nomes)
     {
-        Console.WriteLine();
+        Console.WriteLine($"Foi adicionado '{quantidadeJogos}' jogos: {string.Join(", " , nomes)} ao catalogo.");
     }
 }
